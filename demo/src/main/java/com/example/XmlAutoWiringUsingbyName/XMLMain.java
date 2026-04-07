c6420704d34f0ea767acc0e45ecc5c7f43c0598a
Bappi Singh <bappi.r.singh@gmail.com>
2026-04-07 19:51:15 +0530

    XML autoconfiguration using autowire attribute (byName)

---
+++ demo/src/main/java/com/example/XmlAutoWiringUsingbyName/XMLMain.java
@@ -0,0 +1,13 @@
+package com.example.XmlAutoWiringUsingbyName;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XMLMain {
+    public static void main(String[] args) {
+        String config_File = "applicationContext.xml";
+        ApplicationContext context = new ClassPathXmlApplicationContext(config_File);
+        Student std  = (Student)  context.getBean("std");
+        std.display();
+    }
+}
